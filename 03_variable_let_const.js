fad2e049839b190449eada5e2460340917a85608
tanzim <mdtanzimehsan@gmail.com>
2023-01-31 01:41:41 +0600

    variables

---
+++ 03_variable_let_const.js
@@ -0,0 +1,13 @@
+console.log("javascript:var,let,const")
+let person ="tqanzim", price = 100, friend ="tahjib" 
+console.log(person,price,friend)
+let bubt
+console.log(bubt)
+var tanzim ="i"
+var tanzim ="l"//latest jeta declare korbo sheta show korbe output a.In var we can redeclare 
+console.log(tanzim)
+//but in let and cons we can not re declare the program will not working
+//let a="gg"
+//let a
+//console.log(a)
+//Const:always declare variable with const.If the value of the variable can change use let
