29624ed48e01857c09d66a64b40932fd538c7402
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 15:03:12 +0600

    practice 01-04

---
+++ 05_practice.js
@@ -0,0 +1,22 @@
+//problem set 1
+let a="tanzim"
+let b =7
+console.log(a+b)
+//Qustion ans two
+console.log(typeof (a+b))
+
+//Question ans 3
+//create a const object
+const mark = {
+  1:"bangla",
+  2:"english",
+  3:"hindi"
+}
+console.log(mark[1])
+
+//question ans 4
+//add and update objects elements 
+mark[4]="german"
+console.log(mark)
+mark[3]=1
+console.log(mark)
