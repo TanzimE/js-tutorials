79456c8c302d220a78ecdd81177b76c4a2e74d40
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 15:00:58 +0600

    primitive and object

---
+++ 04_primitive_objects.js
@@ -0,0 +1,39 @@
+//nn bb ss u
+let a = null//NULL
+let b = 123//number
+let c = true//boolean
+let d = BigInt("567")//BigInt
+let e ="tanzim"//String
+let f = Symbol("this is a symbol")//Symbol
+let g = undefined//Undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof f)//if we want to see variable's data type follow this step
+
+//objects In js
+const item ={
+   true: "tanzim",
+  "ehsan":false,
+  "zihad": 61,"md":undefined
+
+}
+console.log(item.true)
+let value = {
+  
+  1: "tanzim",
+  "Ehsan":2,
+  3:"Zihad",
+  
+}
+  console.log(value[1])
+//number premitive:
+//example:
+let x = 250;
+let y = 40.5;
+console.log(x+y);
+console.log(y);
+
+//string
+let str = 'Hello All';
+let str1 = "Welcome to my new house";
+console.log("Value of str=" + str);
+console.log("Value of str1=" + str1);
