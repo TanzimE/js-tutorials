52c643306a37e87ab795617ba295f663ccd4bd61
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 17:02:36 +0600

    While loop

---
+++ 09_for_loops.js
@@ -0,0 +1,28 @@
+/*let sum = 1
+let n = prompt("enter the value:")
+n = Number.parseInt(n)
+for (let i =0 ;i<n;i++){
+  sum = sum + i
+  //console.log(sum + i,"+")
+}
+console.log(sum)
+
+*/
+//FOR IN LOOPS
+let run = {
+    shakib:12,
+    tamim:50,
+    rony:1,
+    shanto:0
+  }
+  for (let a in run){
+    console.log(a)
+    console.log("Player run " +  a + " "  + run[a])
+  }
+  
+  //FOR OF OOPS 
+  /*
+  for (let a of "My Name Is Tanzim"){
+    console.log(a)
+  }
+  */
---
+++ 10_while loops.js
@@ -0,0 +1,21 @@
+//WHILE LOOPS
+/*let n =prompt("enter a number")
+n=Number.parseInt(n)
+let i = 0;
+while (i<n){
+  console.log(i)
+ i++
+}*/
+
+
+//DO WHILE LOOP
+
+let n = prompt("Enter a number")
+n=Number.parseInt(n)
+let i=0;
+do
+{
+console.log(i)
+  i++
+}
+while (i<n)
