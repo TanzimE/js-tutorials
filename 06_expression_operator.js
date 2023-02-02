f1507a55410d2d588c824ff3a99bc20d4670b464
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 15:04:09 +0600

    expression and operator

---
+++ 06_expression_operator.js
@@ -0,0 +1,14 @@
+//Javascript String operators
+let a="Tanzim"
+let b=" Ehsan"
+let c= a+b
+console.log(c)
+//
+x=5^1
+console.log(x)
+//
+let cars1 =["Saab", "volvo","BMW"];
+let cars2 = ["Fiat","Toyota"];
+let combined = [cars1,... cars2];
+console.log(combined)
+//
