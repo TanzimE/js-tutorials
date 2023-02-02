4b82a0fc2adaac70dcf63a376a8440e11a3811cc
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 15:05:24 +0600

    practice 6-7

---
+++ 08_practice.js
@@ -0,0 +1,62 @@
+//Problem no 1-(If-else problem)
+/*let age = prompt("What is your age?")
+age= Number.parseInt(age)
+if(age>=10 && age<=20){
+  alert("Age between 10 to 20")
+}
+else{
+  alert("this  is lie")
+}
+console.log(age)
+
+*/
+//Problem no 2 -(SWITCH case problem:)
+
+/*let age = prompt("What is your age?")
+age= Number.parseInt(age)
+switch (age){
+  case '12':
+    console.log("your age is 12")
+    break
+  case '13':
+    console.log("Your age 13")
+  default:
+    console.log("Your age is not special")
+}
+*/
+
+//problem 3-(//Is no  is divisible by 2 and 3 ?)
+
+/*let number = prompt("Enter a number:")
+number=Number.parseInt(number)
+if(number%2==0 && number%3==0)
+{
+  console.log("number is divisible by 2 and 3")
+}
+else
+{
+  console.log("number is not divisible by 2 nad 3")
+}  
+console.log(number)
+*/
+
+//problem 4-(//Is no  is divisible by 2 or 3 ?)
+
+/*let number = prompt("Enter a number:")
+number=Number.parseInt(number)
+if(number%2==0 || number%3==0)
+{
+  console.log("number is divisible by 2 and 3")
+}
+else
+{
+  console.log("number is not divisible by 2 nad 3")
+}  
+console.log(number)
+*/
+
+//problem 5-(TERNARY OPERATOR)
+//TERNARY OPERATOR
+let age = 19
+let a =age > 18 ? "You can drive"  : "You can not drive" 
+console.log(a)
