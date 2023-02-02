abe1762ff6c716db4e9259ab227b456f5356a16f
tanzim <mdtanzimehsan@gmail.com>
2023-02-02 17:41:10 +0600

    Function

---
+++ 11_Function.js
@@ -0,0 +1,24 @@
+/*function score(a,b){
+  console.log("This is the solution")
+  return 1+a+b  
+  
+}*/
+/*const score =(p,q)=>{
+  return p+q
+}
+let x=1
+let y =2
+let z=3
+console.log(score(x,y))
+console.log(score(y,z))
+*/
+const a =()=>{
+    console.log("My name is tanzim")
+    return "hello"
+      
+    }
+  
+  let e = a()
+  console.log(e)
+    
+    
