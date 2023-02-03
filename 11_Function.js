6834cf88c47d0a46c24be0c90790e23c19d76ea3
tanzim <mdtanzimehsan@gmail.com>
2023-02-03 20:02:04 +0600

    String Intro

--- 11_Function.js
+++ 11_Function.js
@@ -3,7 +3,7 @@
   return 1+a+b  
   
 }*/
-/*const score =(p,q)=>{
+const score =(p,q)=>{
   return p+q
 }
 let x=1
@@ -11,7 +11,7 @@ let y =2
 let z=3
 console.log(score(x,y))
 console.log(score(y,z))
-*/
+
 const a =()=>{
     console.log("My name is tanzim")
     return "hello"
