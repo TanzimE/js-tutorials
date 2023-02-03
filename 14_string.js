12a297a356c083ca030685b62ed37115bc6705bc
tanzim <mdtanzimehsan@gmail.com>
2023-02-03 20:03:20 +0600

    String

---
+++ 14_string.js
@@ -0,0 +1,10 @@
+//Template literals
+let boy1 ="borshon"
+let  boy2="sezan"
+//borshon is a friend of sezan
+let sentence =`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+ //Escape sequence  Characters 
+let fruit = 'bana\'na'
+console.log(fruit.length)
+console.log(fruit)
