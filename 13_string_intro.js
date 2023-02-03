c803498c1e6a413b9010ae00e7b861fc2aae167a
tanzim <mdtanzimehsan@gmail.com>
2023-02-03 20:00:16 +0600

    string intro

---
+++ 13_string_intro.js
@@ -0,0 +1,18 @@
+let name ="tanzim"
+console.log(name)
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+let friend ='tahjib'
+console.log(friend)
+
+//Template literals
+let boy1 ="borshon"
+let  boy2="sezan"
+//borshon is a friend of sezan
+let sentence =`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+ //Escape sequence  Characters 
+let fruit = 'bana\'na'
+console.log(fruit.length)
+console.log(fruit)
